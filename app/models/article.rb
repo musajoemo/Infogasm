0e19f3ccbc99d9a886dbe770c031aa982a16d91b
MTen <MichaelTener@gmail.com>
2013-11-12 00:02:59 -0500

    Done for today - minor modifications to article and user models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,11 @@
 class Article < ActiveRecord::Base
 	validates_presence_of :title
+validates_uniquess_of :asset_id
 
 	belongs_to :user
 
 	def long_title
-		"#{title} - #{published_at}"
+		"#{title} - #{published_at}" 
 	end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ActiveRecord::Base
 	validates_uniquess_of :email
 
 	validates_length_of :email, :within 5..75
+	validates_format_of :email, :with => /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$/i
+	validates_confirmation_of :password
 
 	has_one :profile
 	has_many :articles
 end
+
